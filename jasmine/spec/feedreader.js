67c8e3e8991893a41c3c59587b54dad11588ffca
danodea <danodea@gmail.com>
2015-07-19 13:49:04 -0400

    Fix readme, comments, and clean up tests

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -8,10 +8,12 @@
  * since some of these tests may require DOM elements. We want
  * to ensure they don't run until the DOM is ready.
  */
+
 $(function() {
-    
+
+    // These tests make sure the RSS feeds object is created properly
     describe('RSS Feeds', function() {
-        
+
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
@@ -40,7 +42,7 @@ $(function() {
                 expect(feed.url.length).not.toBe(0);
             });
         };
-        
+
         for (var i = 0; i < allFeeds.length; i++) {
             testThatUrlIsDefined(allFeeds[i], i)
         };
@@ -51,19 +53,22 @@ $(function() {
                 expect(feed.name.length).not.toBe(0);
             });
         };
-        
+
         for (var i = 0; i < allFeeds.length; i++) {
             testThatNameIsDefined(allFeeds[i], i)
         };
     });
 
-
+    // These tests make sure the menu is working properly
     describe('The menu', function() {
-
+        // Make sure the body has the css to hide the menu by default.
+        // We can only test that the code is working properly.
+        // To make sure that the code is doing what we want visually,
+        // you have to actually load the page and manually check
         it('should be hidden by default', function() {
             expect($('body').hasClass('menu-hidden')).toBe(true);
         });
-
+        // Make sure the hiding class actually toggles on click
         it('should hide and show when clicked', function() {
             $('.menu-icon-link').trigger('click');
             expect($('body').hasClass('menu-hidden')).not.toBe(true);
@@ -73,33 +78,37 @@ $(function() {
 
     });
 
-
+    // These tests make sure the app loads up correctly
     describe('Initial Entries', function() {
-
+        // Load a feed
         beforeEach(function(done) {
            loadFeed(0, done);
         });
-
-        it('has at least one entry in the feed list', function(done) {
+        // This test makes sure the app loads a feed on startup
+        it('has at least one entry in the feed list', function() {
            expect($('.feed .entry').length).not.toBe(0);
-           done();
         });
 
     });
 
-
+    // This test makes sure selecting new feeds works
     describe('New Feed Selection', function() {
 
         var firstContent;
 
         beforeEach(function(done) {
-            firstContent = $('.feed').html();
-            loadFeed(1, done);
+            // Load a feed
+            loadFeed(0, function() {
+                // Save the content of the first feed
+                firstContent = $('.feed').html();
+                // Load another feed
+                loadFeed(1, done);
+            })
         });
 
-        it('should change the content when a new feed is selected', function(done) {
+        it('should change the content when a new feed is selected', function() {
+            // Compare the content of the new feed to the first feed
             expect(firstContent).not.toBe($('.feed').html());
-            done();
         });
     })
 }());
